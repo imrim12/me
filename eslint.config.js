20dc566bf7bf501a438a80003fbdba9134c67723
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-10-19 13:58:28 +0700

    feat: add cloudflare deployment

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  vue: true,
+  react: true,
   rules: {
     'no-alert': 'warn',
     'no-console': 'warn',
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
